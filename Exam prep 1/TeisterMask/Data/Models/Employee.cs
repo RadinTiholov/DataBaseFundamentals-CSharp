e840d9c74dd477d7151de54d78f2e379a549e0d9
RadinTiholov <radin.tiholov@mail.bg>
2021-11-28 16:12:39 +0200

    Solved the first problem.

---
+++ Exam prep 1/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Username { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(12)]
+        public string Phone { get; set; }
+        public virtual ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
